0b0ceb56056eb43171c2190693d516d9421ec191
Rajnish Kumar <rajnishkr.22.11.48@gmail.com>
2026-01-12 23:42:35 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDWrZeJtkzxMIGUpNOEf7LLta5FA5GQ82Q",
-  authDomain: "faithconnect-1aa1f.firebaseapp.com",
-  projectId: "faithconnect-1aa1f",
-  storageBucket: "faithconnect-1aa1f.firebasestorage.app",
-  messagingSenderId: "564710310102",
-  appId: "1:564710310102:web:e8ef43faa8c4a6963d8977"
+  keep your firbase config
 };
 const app = initializeApp(firebaseConfig);
 
